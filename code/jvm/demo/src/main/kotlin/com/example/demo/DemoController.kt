05d58887b1c4ce697891f5f9c1a973be1995e4f4
48529 - Vasco Branco <a48259@alunos.isel.pt>
2023-10-03 10:58:40 +0100

    -First commit

---
+++ code/jvm/demo/src/main/kotlin/com/example/demo/DemoController.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/gomoku")
+@RestController
+class Controller(){
+
+
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,3 @@
 Please edit this file with the table of contents for the documentation.
+
+Vasco Branco - 48259
