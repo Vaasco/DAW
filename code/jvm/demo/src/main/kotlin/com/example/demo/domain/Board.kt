e776775348fd693071c61ab5a19c25611e460419
sergio <sergio_e_f_capela@outlook.pt>
2023-10-03 15:23:35 +0100

    board

--- code/jvm/demo/src/main/kotlin/com/example/demo/domain/Board.kt
+++ code/jvm/demo/src/main/kotlin/com/example/demo/domain/Board.kt
@@ -2,16 +2,55 @@ package com.example.demo.domain
 
 const val BOARD_DIM = 5
 
-data class Board(val positions: Map<Cell, Player?>, val turn: Player?) {
-    companion object {
-        val emptyBoard = Board(emptyMap(), null)
+typealias Moves = Map<Cell, Player>
+
+private const val CENTER = (MAX_MOVES / 2 + BOARD_DIM / 2)
+val INITIALMAP get () = mapOf()
+sealed class Board(val moves: Moves) {
+
+    /**
+     * Função "equals" em que servirá para verificar se o objeto é o mesmo e efetuar a verificação de que se trata do mesmo objeto ou não.
+     * @param other que representa o outro objeto com que queremos comparar esse Board com o other.
+     * @return Boolean a representar se se trata do mesmo objeto ou não.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Board) return false
+        if (this::class != other::class) return false
+        return moves.size == other.moves.size
     }
+
+    //Função "hashCode" que será igual ao valor do hashcode de moves.
+    override fun hashCode(): Int = moves.hashCode()
 }
 
+/**
+ * Classe "BoardRun" que representa o tabuleiro de jogo quando este está a ser jogado retornando o objeto do tipo "Board".
+ * @property moves representa os moves desse board.
+ * @property turn representa o turno do jogador que é a jogar ou não.
+ * @return Board representa o Board que representa o nosso BoardRun.
+ */
+class BoardRun(moves: Moves, val turn: Player, val pass: Boolean = false): Board(moves)
 
-fun Board.add(cell: Cell, player: Player): Board {
-    require(cell != Cell.INVALID) { "This position doesn't exist" }
-    return copy(positions = positions + (cell to player), turn = turn?.other())
-}
+/**
+ * Classe "BoardWin" que representa o tabuleiro de jogo quando existe um vencedor desse jogo.
+ * @property moves representa os movimentos efetuados nesse tabuleiro.
+ * @property winner representa o vencedor desse jogo.
+ * @return Board representa o Board quando este acabou com um vencedor.
+ */
+class BoardWin(moves: Moves, val winner: Player): Board(moves)
 
+/**
+ * Classe "BoardDraw" que representa o tabuleiro de jogo quando este empatou.
+ * @property moves representa os movimentos efetuados nesse tabuleiro.
+ * @return Board representa o board quando este acabou em empate.
+ */
+class BoardDraw(moves: Moves) : Board(moves)
+
+/**
+ * Função "createBoard" responsável por criar o novo Board com os dados iniciais.
+ * @param first representa o jogador que o utilizador irá ser nesse board.
+ * @return BoardRun representa o nosso tabuleiro durante um jogo.
+ */
+fun createBoard(first: Player) = BoardRun(INITIALMAP, first)
 
