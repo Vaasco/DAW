f132dd7adee07d888ff4b9ea8345bc59a4b7a3b5
48269-Jose <a48269@alunos.isel.pt>
2023-10-17 16:28:53 +0100

    Changes made to the database

--- code/jvm/demo/src/main/kotlin/com/example/demo/SQL/createTable.sql
+++ code/jvm/demo/src/main/kotlin/com/example/demo/SQL/createTable.sql
@@ -1,15 +1,19 @@
 create table player(
 	id serial,
+    token varchar(30) unique not null,--TODO("Adjust the max length of the token")
 	username varchar(30) unique not null,
-	lobby_id integer,
-	primary key (id),
-	foreign key (lobby_id) references lobby(id)
+	primary key (id)
 );
 
 create table game(
 	id serial,
-	state varchar(30), --TODO(Não temos bem a certeza do que pôr no state)
-	primary key (id)
+	state varchar(7) not null,
+    rules varchar(8) not null,
+    variant varchar(30) not null,
+	primary key (id),
+    constraint invalid_state check(state in ('Playing', 'Ended B', 'Ended W', 'Ended D')),
+    constraint invalid_rules check(rules in ('Pro', 'Long Pro')),--TODO("Add more rules maybe?")
+    constraint invalid_variant check(variant in ('Freestyle', 'Swap after 1st move'))--TODO("Add more variants maybe?"
 );
 
 create table Lobby(
@@ -24,5 +28,3 @@ create table Lobby(
 	constraint game_id foreign key (game_id) references game(id),
 	constraint same_player check(player1_id <> player2_id)
 );
-
-drop table Lobby;
