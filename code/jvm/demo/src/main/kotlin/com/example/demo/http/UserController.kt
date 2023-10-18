8d0a9e5a6fe25f6134cf66ce9703b111fc185da0
48529 - Vasco Branco <a48259@alunos.isel.pt>
2023-10-18 18:47:30 +0100

    -User controller

--- code/jvm/demo/src/main/kotlin/com/example/demo/http/UserController.kt
+++ code/jvm/demo/src/main/kotlin/com/example/demo/http/UserController.kt
@@ -1,6 +1,5 @@
 package com.example.demo.http
 
-import com.example.demo.domain.TempUser
 import com.example.demo.service.UsersService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
