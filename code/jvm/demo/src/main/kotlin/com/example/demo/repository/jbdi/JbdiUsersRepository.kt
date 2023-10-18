432bb4de8f9173c81c32b3e2a531cfb11ea2d036
48529 - Vasco Branco <a48259@alunos.isel.pt>
2023-10-18 19:21:16 +0100

    -Changes by sergi

--- code/jvm/demo/src/main/kotlin/com/example/demo/repository/jbdi/JbdiUsersRepository.kt
+++ code/jvm/demo/src/main/kotlin/com/example/demo/repository/jbdi/JbdiUsersRepository.kt
@@ -1,29 +1,34 @@
 package com.example.demo.repository.jbdi
 
-import com.example.demo.domain.TempUser
 import com.example.demo.repository.UsersRepository
 import org.jdbi.v3.core.Jdbi
 import org.springframework.stereotype.Component
 import java.lang.Exception
+import java.util.UUID
 
 @Component
 class JbdiUsersRepository(private val jdbi: Jdbi) : UsersRepository {
+    //converter a String em user
+
     override fun getById(id: Int): String? {
         return jdbi.withHandle<String?, Exception> { handle ->
             handle.createQuery("select username from player where id = :id")
                 .bind("id", id)
                 .mapTo(String::class.java)
                 .singleOrNull()
+
         }
     }
 
-    override fun createUser(user: TempUser) {
-        val userName = user.name
+    override fun createUser(username: String, password: String) {
+       val token =  UUID.randomUUID().toString()
 
         jdbi.useHandle<Exception> { handle ->
-            val query = "INSERT INTO player(username) VALUES (:userName)"
+            val query = "INSERT INTO player(username, password, token) VALUES (:username, :password, :token)"
             handle.createUpdate(query)
-                .bind("userName", userName)
+                .bind("userName", username)
+                .bind("password", password)
+                .bind("token", token)
                 .execute()
         }
     }
