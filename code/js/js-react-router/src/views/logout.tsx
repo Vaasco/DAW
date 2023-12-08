15b8631a482a7fe38d0c7e296c965d91940351c7
48269-Jose <a48269@alunos.isel.pt>
2023-12-08 15:29:36 +0000

    logout

---
+++ code/js/js-react-router/src/views/logout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {useFetch} from "../utils/useFetch";
+import {Navigate} from "react-router-dom";
+
+export async function Logout() {
+    const fetch = await useFetch("users/logout", "POST", {})
+    const body = await fetch.json()
+
+    return (
+        <div>
+            {body.properties && (
+                <div>
+                    <h1><Navigate to="/" replace={true}/></h1>
+                </div>
+            )}
+        </div>
+    );
+}
